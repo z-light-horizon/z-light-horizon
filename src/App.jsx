7c3ae4a64576c4bf6db6941436e171d9276f73f1
tanxueyi99 <tanxueyi99@gmail.com>
2026-02-25 21:06:57 +0800

    screen size revert, retrict to desktop only

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <>
       {/* Mobile block screen */}
 
-      {/* <div className="flex lg:hidden min-h-screen items-center justify-center bg-black px-6">
+      <div className="flex lg:hidden min-h-screen items-center justify-center bg-black px-6">
         <div className="text-center">
           <div className="text-5xl mb-4">🖥️</div>
           <h1 className="text-2xl font-semibold text-white mb-2">
@@ -25,11 +25,11 @@ function App() {
             Please visit us on a desktop or laptop for the full experience.
           </p>
         </div>
-      </div> */}
+      </div>
 
       {/* Desktop only content */}
-      <div className="flex min-h-screen flex-col bg-black">
-        {/* <div className="hidden lg:flex min-h-screen flex-col bg-black"> */}
+      {/* <div className="flex min-h-screen flex-col bg-black"> */}
+      <div className="hidden lg:flex min-h-screen flex-col bg-black">
         <div className="flex-1">
           <Routes>
             <Route
